22230d6f5d2fc4d6839e772c4659e69468b11f1c
Pratik.Shrivastav <Pratik.Shrivastav@DELL1615>
2024-07-24 15:07:20 +0530

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    static void DoMoreStuff()
+    {
+        int number1 = 1;
+        int number2 = 0;
+        Console.WriteLine(number1 / number2);
+    }
+    static void DoStuff()
+    {
+        DoMoreStuff();
+        Console.WriteLine("Inside Do Stuff");
+    }
+    static void Main(string[] args)
+    {
+        DoStuff();
+        Console.WriteLine("Inside Main");
+    }
+}
